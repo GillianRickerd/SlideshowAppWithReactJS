04ad25d64f3de9109a49db9b98a394253b951e86
Gillian Lemke <gillian.lemke@atomicobject.com>
2017-05-19 13:06:47 -0400

    refactoring

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <img className="logo" src={require("/Users/gillianlemke/AO/slideshow-app/src/img/logo.jpg")} alt="logo"/>
+        <img className="logo" src={require("./img/logo.jpg")} alt="logo"/>
         <h2 className="app-title">Bootcamp Slides App</h2>
       </div>
     )
